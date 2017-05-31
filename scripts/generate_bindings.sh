388770e5a77a38d55d91b51fe9c861d9d51ac8d6
Rob Tsuk <robtsuk@google.com>
2017-05-31 10:51:42 -0700

    Experiment with bindgen
    Change-Id: If9b2c475c34c6e0a9f714d1da27ae2e9e54f59f3

---
+++ scripts/generate_bindings.sh
@@ -0,0 +1,40 @@
+set -e
+#set -x
+
+command -v bindgen >/dev/null 2>&1 || { echo "Error: I require the bindgen command line tool but it's not installed." >&2; exit 1; }
+
+script_dir=$(dirname $BASH_SOURCE)
+script_dir=`cd $script_dir; pwd`
+src_dir=`cd $script_dir/../magenta-sys/src; pwd`
+default_fuchsia_root=`cd ../..;pwd`
+
+: ${FUCHSIA_ROOT=$default_fuchsia_root} && export FUCHSIA_ROOT
+
+if [ ! -d "$FUCHSIA_ROOT" ] ; then
+  echo "Error: Fuchsia root directory '$FUCHSIA_ROOT' does not exist." >&2
+  exit 1
+fi
+
+
+: ${FUCHSIA_SYSROOT=$FUCHSIA_ROOT/out/debug-x86-64/sysroot} && export FUCHSIA_SYSROOT
+
+if [ ! -d "$FUCHSIA_SYSROOT" ] ; then
+  echo "Error: Fuchsia sysroot '$FUCHSIA_SYSROOT' does not exist." >&2
+  exit 1
+fi
+
+temp_file_prefix=`basename $0`
+TEMP_FILE=`mktemp -t ${temp_file_prefix}`.h || { echo "Error: Can't create temporary file." >&2; exit 1; }
+
+# bindgen $FUCHSIA_SYSROOT/include/magenta/types.h --whitelist-var MX_.* --whitelist-type mx_.* -o $src_dir/magenta_types.rs -- -I$FUCHSIA_SYSROOT/include
+# bindgen $FUCHSIA_SYSROOT/include/magenta/processargs.h --whitelist-var PA.* -o $src_dir/processargs.rs -- -I$FUCHSIA_SYSROOT/include
+# bindgen $FUCHSIA_SYSROOT/include/magenta/process.h --whitelist-function mx_.* -o $src_dir/process.rs -- -I$FUCHSIA_SYSROOT/include
+bindgen $script_dir/all_headers.h \
+    -o $src_dir/generated_bindings.rs \
+    --no-layout-tests \
+    --with-derive-default -- \
+    -I$FUCHSIA_SYSROOT/include \
+    -Wno-unknown-attributes \
+    -std=c11
+
+#rm $TEMP_FILE
